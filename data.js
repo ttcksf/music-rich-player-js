bd3ab96dd3f73b204266a82c1df5b6cbd3b1a10e
ttcksf <ttc0104ksf1993@yahoo.co.jp>
2022-12-16 05:23:35 +0900

    firstt

---
+++ data.js
@@ -0,0 +1,50 @@
+let songs = [
+  {
+    name: "friends",
+    path: "assets/musics/friends.mp3",
+    artist: "iri",
+    cover: "assets/images/friends.jpeg",
+  },
+  {
+    name: "24-25",
+    path: "assets/musics/24-25.mp3",
+    artist: "iri",
+    cover: "assets/images/24-25.jpeg",
+  },
+  {
+    name: "言えない",
+    path: "assets/musics/ienai.mp3",
+    artist: "iri",
+    cover: "assets/images/ienai.jpeg",
+  },
+  {
+    name: "摩天楼",
+    path: "assets/musics/matenro.mp3",
+    artist: "iri",
+    cover: "assets/images/matenro.jpeg",
+  },
+  {
+    name: "Sparkle",
+    path: "assets/musics/sparkle.mp3",
+    artist: "iri",
+    cover: "assets/images/Sparkle.jpeg",
+  },
+  {
+    name: "STARLIGHTS",
+    path: "assets/musics/starlights.mp3",
+    artist: "iri",
+    cover: "assets/images/starlights.jpeg",
+  },
+  {
+    name: "渦",
+    path: "assets/musics/uzu.mp3",
+    artist: "iri",
+    cover: "assets/images/uzu.jpeg",
+  },
+  {
+    name: "ナイトグルーヴ",
+    path: "assets/musics/nightgroove.mp3",
+    artist: "iri",
+    cover: "assets/images/nightgroove.jpeg",
+  },
+];
